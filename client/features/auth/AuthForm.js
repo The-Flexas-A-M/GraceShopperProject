973a9eaf6224e4eb8ab37063897a0f174be29071
Declan Noetzelman <declan@noetzelman.com>
2023-06-15 15:55:07 -0400

    login/signup skeleton done

--- client/features/auth/AuthForm.js
+++ client/features/auth/AuthForm.js
@@ -22,24 +22,69 @@ const AuthForm = ({ name, displayName }) => {
 
   return (
     <div>
-      <form onSubmit={handleSubmit} name={name}>
-        <div>
-          <label htmlFor="username">
-            <small>Username</small>
-          </label>
-          <input name="username" type="text" />
-        </div>
-        <div>
-          <label htmlFor="password">
-            <small>Password</small>
-          </label>
-          <input name="password" type="password" />
-        </div>
-        <div>
-          <button type="submit">{displayName}</button>
-        </div>
-        {error && <div> {error} </div>}
-      </form>
+        {
+          window.location.pathname === "/signup" ? (
+            <form onSubmit={handleSubmit} name={name} id="signin-form">
+              <h1>Don't have an account? Sign up.</h1>
+              <div class="input-container">
+                <label htmlFor="firstname">
+                  <small>First Name</small>
+                </label>
+                <input name="firstname" type="text" required/>
+              </div>
+                <div class="input-container">
+                <label htmlFor="lastname">
+                  <small>Last Name</small>
+                </label>
+                <input name="Last Name" type="text" required/>
+              </div>
+              <div class="input-container">
+                <label htmlFor="email">
+                  <small>Email</small>
+                </label>
+                <input name="email" type="text" required/>
+              </div>
+              <div class="input-container">
+                <label htmlFor="username">
+                  <small>Username</small>
+                </label>
+                <input name="username" type="text" required/>
+              </div>
+              <div class="input-container">
+                <label htmlFor="password">
+                  <small>Password</small>
+                </label>
+                <input name="password" type="password" required/>
+              </div>
+              <div class="input-container">
+                <button type="submit">{displayName}</button>
+              </div>
+              {error && <div> {error} </div>}
+              <a href="/login">Have an account? Log in.</a>
+            </form>
+          ) : (
+            <form onSubmit={handleSubmit} name={name} id="signin-form">
+              <h1>Have an existing account? Log in.</h1>
+              <div class="input-container">
+                <label htmlFor="username">
+                  <small>Username</small>
+                </label>
+                <input name="username" type="text" />
+              </div>
+              <div class="input-container">
+                <label htmlFor="password">
+                  <small>Password</small>
+                </label>
+                <input name="password" type="password" />
+              </div>
+              <div class="input-container">
+                <button type="submit">{displayName}</button>
+              </div>
+              {error && <div> {error} </div>}
+              <a href="/signup">Don't have one? Sign up here</a>
+            </form>
+          )
+        }
     </div>
   );
 };
