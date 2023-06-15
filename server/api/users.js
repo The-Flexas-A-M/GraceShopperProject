c04fac1a17c2ee0d36c645d10f343c206408787e
KelvinBenitez <kelvinbenitez22@gmail.com>
2023-06-15 11:29:33 -0400

    create-route-to-create-users

--- server/api/users.js
+++ server/api/users.js
@@ -1,6 +1,5 @@
 const router = require('express').Router()
 const { models: { User }} = require('../db')
-module.exports = router
 
 router.get('/', async (req, res, next) => {
   try {
@@ -15,3 +14,49 @@ router.get('/', async (req, res, next) => {
     next(err)
   }
 })
+router.post('/', async (req, res, next) => {
+  try {
+    console.log('Request Payload:', req.body); // Log the request payload
+
+    // Create a new user using the request body
+    const newUser = await User.create(req.body);
+
+    // Only send the necessary user details in the response
+    const { id, username } = newUser;
+
+    res.status(201).json({ id, username });
+  } catch (error) {
+    next(error);
+  }
+});
+
+// DELETE /api/users/:userId
+router.delete('/:userId', async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.params.userId);
+    await user.destroy();
+    res.send(user);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// PUT /api/users/:userId
+router.put('/:userId', async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.params.userId);
+
+    // Update the user attributes based on the request body
+    await user.update(req.body);
+
+    // Only send the necessary user details in the response
+    const { id, username } = user;
+
+    res.json({ id, username });
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
+
