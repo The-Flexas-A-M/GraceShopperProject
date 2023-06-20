06e1033bd62186fd4b01e85ca42592343d843d9d
rend1027 <rend1027@yahoo.com>
2023-06-19 23:17:58 -0400

    add QTY select button

--- client/features/cart/CartItem.js
+++ client/features/cart/CartItem.js
@@ -4,18 +4,29 @@ import {
   CardActions,
   CardContent,
   CardMedia,
+  InputLabel,
+  MenuItem,
+  Select,
   Typography,
 } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { removeCartItem } from "./cartItemSlice";
 
 const CartItem = ({ item }) => {
   const dispatch = useDispatch();
   // console.log("this is item ----->", item);
+  const [quantity, setQuantity] = useState(item.quantity);
+
   const handleRemoveClick = () => {
     dispatch(removeCartItem(item.id));
   };
+
+  const handleChange = (event) => {
+    setQuantity(event.target.value);
+    // TODO: Dispatch action to update quantity in the cart
+  };
+
   return (
     <Card sx={{ border: "1px solid grey" }}>
       <CardContent>
@@ -29,6 +40,17 @@ const CartItem = ({ item }) => {
         <Typography variant="h6">{`$${item.product.price}`}</Typography>
       </CardContent>
       <CardActions>
+        <InputLabel id="quantity-button-label">QTY</InputLabel>
+        <Select
+          labelId="quantity-button-label"
+          id="quantity-select"
+          value={quantity}
+          onChange={handleChange}
+        >
+          <MenuItem value={1}>1</MenuItem>
+          <MenuItem value={2}>2</MenuItem>
+          <MenuItem value={3}>3</MenuItem>
+        </Select>
         <Button size="small" color="primary" onClick={handleRemoveClick}>
           Remove
         </Button>
