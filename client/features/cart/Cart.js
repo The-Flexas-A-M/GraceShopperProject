31eb6db86591d97219f3d89ac75c8a43a2bbb0b2
rend1027 <rend1027@yahoo.com>
2023-06-20 13:45:02 -0400

    Create OrderSummary component and add to Cart

--- client/features/cart/Cart.js
+++ client/features/cart/Cart.js
@@ -2,14 +2,16 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchCartItems } from "./cartItemSlice";
 import CartItem from "./CartItem";
+import { Box } from "@mui/material";
+import OrderSummary from "./OrderSummary";
 
 const Cart = () => {
-  const dispatch = useDispatch(); 
+  const dispatch = useDispatch();
   const auth = useSelector((state) => state.auth);
-  const userId = auth.me ? auth.me.id : null; 
-  const cartItems = useSelector((state) => state.cartItem.cartItem); 
-  const cartStatus = useSelector((state) => state.cartItem.status); 
-  const error = useSelector((state) => state.cartItem.error); 
+  const userId = auth.me ? auth.me.id : null;
+  const cartItems = useSelector((state) => state.cartItem.cartItem);
+  const cartStatus = useSelector((state) => state.cartItem.status);
+  const error = useSelector((state) => state.cartItem.error);
 
   // console.log("this is auth----->", auth); // test
   // console.log("this is user id---->", userId); // test
@@ -34,12 +36,21 @@ const Cart = () => {
   }
 
   return (
-    <div>
-      Cart Is Working
-      {cartItems.map((item) => (
-        <CartItem item={item} key={item.id} />
-      ))}
-    </div>
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "space-between",
+        marginTop: "1rem",
+      }}
+    >
+      <div>
+        Cart Is Working
+        {cartItems.map((item) => (
+          <CartItem item={item} key={item.id} />
+        ))}
+      </div>
+      <OrderSummary />
+    </Box>
   );
 };
 export default Cart;
