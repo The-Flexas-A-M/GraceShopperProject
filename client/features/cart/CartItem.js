4ecb19a626fbc866126643a242f328658376d17e
“Brendan <Brendan.nieves@gmail.com>
2023-06-21 16:12:07 -0400

    added checkout component

--- client/features/cart/CartItem.js
+++ client/features/cart/CartItem.js
@@ -11,39 +11,42 @@ import {
 } from "@mui/material";
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { removeCartItem, fetchCartItems, updateCartItem} from "./cartItemSlice";
+import { removeCartItem, fetchCartItems, updateCartItem } from "./cartItemSlice";
 
-const CartItem = ({ item }) => {
+const CartItem = ({ item, checkout }) => {
   const auth = useSelector((state) => state.auth);
   const userId = auth.me ? auth.me.id : null;
   const dispatch = useDispatch();
   const [quantity, setQuantity] = useState(item.quantity);
 
 
-  
+
   const handleRemoveClick = () => {
     console.log('this is item product ID ----->', item.product.id);  //
-    dispatch(removeCartItem({userId: userId, productId: item.product.id}))
-    .unwrap()
-    .then(({ userId }) => {
-      dispatch(fetchCartItems(userId)); // fetch updated cart items
-    });
-};
+    dispatch(removeCartItem({ userId: userId, productId: item.product.id }))
+      .unwrap()
+      .then(({ userId }) => {
+        dispatch(fetchCartItems(userId)); // fetch updated cart items
+      });
+  };
 
   const handleChange = (event) => {
     setQuantity(event.target.value);
-    dispatch(updateCartItem({ userId, productId: item.product.id, quantity: event.target.value}))
-    .unwrap()
-    .then(({ userId }) => {
-      dispatch(fetchCartItems(userId)); // fetch updated cart items
-    });
+    dispatch(updateCartItem({ userId, productId: item.product.id, quantity: event.target.value }))
+      .unwrap()
+      .then(({ userId }) => {
+        dispatch(fetchCartItems(userId)); // fetch updated cart items
+      });
   };
 
   return (
     <Card sx={{ border: "1px solid grey" }}>
       <CardContent>
         <Typography variant="h5">{item.product.name}</Typography>
-        <Typography variant="body2">{item.product.description}</Typography>
+        {!checkout ?
+          <Typography variant="body2">{item.product.description}</Typography>
+          : <></>
+        }
         <CardMedia
           component="img"
           image={item.product.imageUrl}
--- client/features/cart/Checkout.js
+++ client/features/cart/Checkout.js
@@ -1,10 +1,67 @@
-import React from 'react'
+import React, { useState, useEffect, useMemo } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addToCart, fetchCartItems, updateCartItem} from "../cart/cartItemSlice";
+import { Box, Card, CardContent, Typography, Button } from "@mui/material";
+import CartItem from "./CartItem";
+import OrderSummary from "./OrderSummary";
+
+
+
 
 const Checkout = () => {
+const auth = useSelector((state) => state.auth);
+const user = useSelector((state) => state.auth.me);
+
+  const userId = auth.me ? auth.me.id : null;
+  const dispatch = useDispatch();
+  const cartItems = useSelector((state) => state.cartItem.cartItem);
+  const cartStatus = useSelector((state) => state.cartItem.status);
+  const error = useSelector((state) => state.cartItem.error);
+//   const [quantity, setQuantity] = useState(item.quantity);
+const subtotal = useMemo(() => {
+    return cartItems.reduce(
+      (acc, item) => acc + item.product.price * item.quantity,
+      0
+    );
+  }, [cartItems]);
+
+
+useEffect(() => {
+    console.log("user", user)
+  }, [user])
+
+  useEffect(() => {
+    if (userId) {
+      dispatch(fetchCartItems(userId));
+    }
+  }, [userId, dispatch]);
+
   return (
-    <div>Checkout</div>
+    <>
+    <Box sx={{display:"flex"}}>
+    <Box sx={{width:"70%"}}>
+        <Box sx={{display:"flex", justifyContent:"space-between", marginTop:"20px", marginLeft:"20px"}}>
+            <div>Shipping Address</div>
+            <div>
+            <div>{user.firstname + " " + user.lastname}</div>
+            <div>{user.street}</div>
+            <div>{user.city + ", " + user.state + " " + user.zip}</div>
+            </div>
+            <Button>Change</Button>
+        </Box>
+        <Box sx={{display:"flex", justifyContent:"space-between", marginTop:"20px", marginLeft:"20px"}}>
+            <div>Payment Method</div>
+            <Button>Add Payment Method</Button>
+        </Box>
+        <Box sx={{marginTop:"20px"}}>{cartItems && cartItems.map((item) => (
+          <CartItem checkout={true} item={item} key={item.id} />
+        ))}
+        </Box>
+    </Box>
+    <Box sx={{display:"flex", justifyContent:"center", width:"30%"}}><div><OrderSummary checkout={true} subtotal={subtotal} />
+</div></Box>
+    </Box>
+    </>
   )
 }
 
