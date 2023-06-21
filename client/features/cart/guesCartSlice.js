ed577c713d61e943f364b4253550e5f983d70ebf
rend1027 <rend1027@yahoo.com>
2023-06-21 14:21:55 -0400

    create guest cart slice and implement in store

---
+++ client/features/cart/guesCartSlice.js
@@ -0,0 +1,41 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = JSON.parse(localStorage.getItem("guestCart")) || [];
+
+export const guestCartSlice = createSlice({
+  name: "guestCart",
+  initialState,
+  reducers: {
+    addToGuestCart: (state, action) => {
+      const existingProduct = state.find(
+        (item) => item.id === action.payload.id
+      );
+      if (existingProduct) {
+        existingProduct.quantity += 1;
+      } else {
+        state.push({ ...action.payload, quantity: 1 });
+      }
+      localStorage.setItem("guestCart", JSON.stringify(state));
+    },
+    removeFromGuestCart: (state, action) => {
+      const index = state.findIndex((item) => item.id === action.payload.id);
+      if (index !== -1) {
+        const product = state[index];
+        product.quantity -= 1;
+        if (product.quantity <= 0) {
+          state.splice(index, 1);
+        }
+      }
+      localStorage.setItem("guestCart", JSON, stringify(state));
+    },
+    clearGuestCart: (state) => {
+      state = [];
+      localStorage.setItem("guestCart", JSON.stringify(state));
+    },
+  },
+});
+
+export const { addToGuestCart, removeFromGuestCart, clearGuestCart } =
+  guestCartSlice.actions;
+
+export default guestCartSlice.reducer;
