720fc07ba754ef0534439d9c79112a060fb913f9
“Brendan <Brendan.nieves@gmail.com>
2023-06-21 21:23:39 -0400

    Finished place order page

--- client/features/cart/Complete.js
+++ client/features/cart/Complete.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { Box, Card, CardContent, Typography } from "@mui/material";
+import { useSelector } from "react-redux";
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
 
 const Complete = () => {
-  return (
-    <div>Complete</div>
-  )
-}
+  const user = useSelector((state) => state.auth.me);
 
-export default Complete
+
+  return (
+    <Box sx={{ maxWidth: 400, mx: 'auto', mt: 4 }}>
+      <Card>
+        <CardContent>
+          <Typography variant="h5" gutterBottom>
+            Thanks for your order!
+          </Typography>
+          <Typography variant="h6" gutterBottom>
+            Hi {user.firstname},
+          </Typography>
+          <Typography variant="body1" gutterBottom>
+            Thank you for shopping with us! We are happy you have found something you like. As soon as your package has shipped, you will receive a confirmation email. Happy gaming!
+            <SportsEsportsIcon sx={{ fontSize: 30, verticalAlign: 'middle' }} />
+          </Typography>
+        </CardContent>
+      </Card>
+    </Box>
+  );
+};
+
+export default Complete;
+
